20730c7551e485cdf6f509627c122ca4213a6e17
roberto <robertox92@gmail.com>
2023-11-26 11:56:43 +0100

    add AppDependency and Versions

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+
+    const val coreKtx = "1.12.0"
+    const val LifecycleRuntime = "2.3.1"
+    const val ComposeActivity = "1.5.1"
+    const val ComposeBom = "2022.10.00"
+    const val KotlinVersion = "1.8.0"
+
+    const val JUnitVersion = "4.13.2"
+
+    const val JUnitExt = "1.1.5"
+    const val EspressoCore = "3.5.1"
+
+}
+
