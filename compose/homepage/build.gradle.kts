9e03193c95b119ca8343e19042da395d4c87c4a5
roberto <robertox92@gmail.com>
2023-11-26 11:58:11 +0100

    refactor: use AppDependency in build.gradle.kts

--- compose/homepage/build.gradle.kts
+++ compose/homepage/build.gradle.kts
@@ -40,22 +40,20 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation (AppDependency.androidXCore)
+    implementation (platform(AppDependency.kotlinBom))
 
-    implementation (platform("androidx.compose:compose-bom:2022.10.00"))
-    implementation ("androidx.compose.ui:ui")
-    implementation ("androidx.compose.ui:ui-graphics")
-    implementation ("androidx.compose.ui:ui-tooling-preview")
-    implementation ("androidx.compose.material3:material3")
+    implementation (platform(AppDependency.composeBom))
+    implementation (AppDependency.composeUi)
+    implementation (AppDependency.composeUiGraphics)
+    implementation (AppDependency.composeUiToolingPreview)
+    implementation (AppDependency.composeMaterial3)
 
-    androidTestImplementation (platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
-    debugImplementation ("androidx.compose.ui:ui-tooling")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    testImplementation (AppDependency.jUnit)
+    androidTestImplementation (AppDependency.jUnitExt)
+    androidTestImplementation (AppDependency.espressoCore)
+    androidTestImplementation (platform(AppDependency.composeBom))
+    androidTestImplementation (AppDependency.composeJUnit)
+    debugImplementation (AppDependency.composeUiTooling)
+    debugImplementation (AppDependency.composeUiTestManifest)
 }
