f8ab4fe115c274a725a3d3494ee50a01d6ca3ae5
roberto <robertox92@gmail.com>
2023-11-26 17:00:58 +0100

    refactor: update build.gradle.kts with AppPlugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id(AppPlugin.androidApp) version Versions.gradleAndroid apply false
+    id(AppPlugin.androidLib) version Versions.gradleAndroid apply false
+    id(AppPlugin.kotlinAndroid) version Versions.KotlinVersion apply false
 }
